028d1efee363a45f69459bf5f098d52cbbcdf525
AmineKh14 <khamouda123@gmail.com>
2019-09-21 22:04:07 +0100

    linking log in page

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\HamoudaKh\Desktop\Projects\Talk_Website\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
