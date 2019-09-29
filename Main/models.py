affbd70e11bc9828c4213ad79433d074dba31674
AmineKh14 <khamouda123@gmail.com>
2019-09-29 21:07:12 +0100

    profile pic added

--- Main/models.py
+++ Main/models.py
@@ -5,5 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile (models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
+    image = models.ImageField(null=True,upload_to='profile_pics',default='profile_pics/man.png')
     
+    def __str__(self):
+        return self.user.username
